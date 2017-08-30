37503b8f4151f36d87b307c068d7fce04174d6ab
Abhishek Das <abhishek.das149@gmail.com>
2017-08-30 04:37:50 -0400

    modified user.js file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,33 +1,30 @@
-$document.ready(function(){
-  Stripe.setPublishableKey($('meta[name="stripe_key"]').attr('content'));
-  //Watch forform submission
-  $("#form-submit-btn").click(function(event){
+$(document).ready(function(){
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  $('#form-submit-btn').click(function(event){
     event.preventDefault();
-    $('input[type=submit]').prop('disabled', true);
-    var error = false;
+    $('input[type=submit]').prop("disbled", true);
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
-        expYear = $('#card_year').val();
-    if(!error)  {
+        expYear = $('#card_year').val()
+    
+    if (!error){
       Stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
         exp_month: expMonth,
         exp_year: expYear
       }, stripeResponseHandler);
-       
-    }
-      return false;
+      
+    } return false;
+    
   });
-  
-  function stripeResponseHandle(status, response) {
-    var f = $("#new_user");
-    var token = response.id
-    //add the tokento the form:
-    f.append('input type="hidden" name="user[stripe_card_token]" value="'+ token +'"/>');
-    //submit the form
+  function stripeResponseHandler(status, response){
+    var f = $('#new_user')
+    var token = response.id; 
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '"/>');
+    
     f.get(0).submit();
+    
   }
-  
 });
